dc85ed0ce94f7a6210ca10404e52a5face6cb1e3
Marek Meissner <marek.meissner77@gmail.com>
2019-10-01 11:26:11 +0200

    total armagedon

--- app/frontend/__tests__/App-test.js
+++ app/frontend/__tests__/App-test.js
@@ -9,6 +9,4 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  
-});
+it('renders correctly', () => {});
