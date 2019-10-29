ca87bb76cc4642e90b132994455142024d6049ab
Marek Meissner <marek.meissner77@gmail.com>
2019-10-29 22:37:44 +0100

    test change

--- app/src/screens/Auth/ForgotPassword/__tests__/ForgotPassword.test.tsx
+++ app/src/screens/Auth/ForgotPassword/__tests__/ForgotPassword.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { renderWithRedux as render } from '@util/test/testRenderers'
+import { renderWithRedux as render } from '../../../../Utils/test/testRenderers'
 import ForgotPassword from '../ForgotPassword'
 
 describe('<ForgotPassword/>', () => {
--- app/src/screens/Auth/Login/__tests__/Login.test.tsx
+++ app/src/screens/Auth/Login/__tests__/Login.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { renderWithRedux as render } from '@util/test/testRenderers'
+import { renderWithRedux as render } from '../../../../Utils/test/testRenderers'
 import Login from '../Login'
 
 describe('<Login/>', () => {
