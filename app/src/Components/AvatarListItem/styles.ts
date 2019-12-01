a1fad083c57fb5a9bfbf963a1f7a5368729ef23d
Marek Meissner <marek.meissner77@gmail.com>
2019-12-01 21:31:09 +0100

    eslint

--- app/src/Components/AvatarListItem/styles.ts
+++ app/src/Components/AvatarListItem/styles.ts
@@ -1,10 +1,9 @@
-import {StyleSheet} from 'react-native'
-import { COLORS } from '@util/styles/colors'
+import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-    listItem: { borderBottomWidth: 0, paddingBottom: 20 },
-    thumbnail: { height: 35, width: 35 },
-    buttonText: { fontSize: 15 }
+  listItem: { borderBottomWidth: 0, paddingBottom: 20 },
+  thumbnail: { height: 35, width: 35 },
+  buttonText: { fontSize: 15 },
 })
 
 export default styles
