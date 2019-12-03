616ef49e457be113b4d44c4686f22036c5f687d8
Marek Meissner <marek.meissner77@gmail.com>
2019-12-03 09:46:40 +0100

    following flow update

--- app/src/Screens/App/PlayerView/PlayerToolkit/PlayerToolkit.tsx
+++ app/src/Screens/App/PlayerView/PlayerToolkit/PlayerToolkit.tsx
@@ -11,20 +11,19 @@ import { User } from '@service/Auth/types'
 
 interface Props {
   audio: Audio
-  followingIds: string[]
   followingFlow: (user: string, following: string[]) => Promise<void>
-  user: User
+  user?: User
 }
 
 const PlayerToolkit: React.FC<Props> = ({ audio, followingIds, followingFlow, user }) => {
   const onFollowButtonPress = async () => {
-    if (followingIds.includes(audio.author.uid)) {
+    if (user && user.following.includes(audio.author.uid)) {
       await followingFlow(
         user.uid,
-        followingIds.filter(uid => uid !== audio.author.uid),
+        user.following.filter(uid => uid !== audio.author.uid),
       )
     } else {
-      await followingFlow(user.uid, followingIds.push(audio.author.uid))
+      user && (await followingFlow(user.uid, user.following.push(audio.author.uid)))
     }
   }
   return (
@@ -51,7 +50,7 @@ const PlayerToolkit: React.FC<Props> = ({ audio, followingIds, followingFlow, us
             </Button>
           </View>
         </View>
-        <AvatarListItem author={audio.author} followingIds={followingIds} followingFlow={onFollowButtonPress} />
+        <AvatarListItem author={audio.author} followingIds={user.following} followingFlow={onFollowButtonPress} />
         <Text style={styles.description}>{audio.details.description}</Text>
       </Content>
       <ShareFab />
@@ -62,7 +61,6 @@ const PlayerToolkit: React.FC<Props> = ({ audio, followingIds, followingFlow, us
 export default connect(
   (state: RootState) => ({
     user: selectUser(state),
-    followingIds: selectUserFollowing(state),
   }),
   { followingFlow },
 )(PlayerToolkit)
