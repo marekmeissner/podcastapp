fa5bde11b22c8ffd0029d1d35f1055653f628c31
Marek Meissner <marek.meissner77@gmail.com>
2020-01-26 13:46:49 +0100

    mock module cleanup

--- app/__mocks__/mockFirebaseAuth.ts
+++ app/__mocks__/mockFirebaseAuth.ts
@@ -2,5 +2,4 @@
 export const config = jest.mock('@react-native-firebase/auth', () => ({
   signInWithEmailAndPassword: jest.fn(),
   sendPasswordResetEmail: jest.fn(),
-  firebase: { auth: () => ({ signInWithEmailAndPassword: jest.fn() }) },
 }))
